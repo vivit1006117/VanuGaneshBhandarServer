0e46b1ae7edd9dd57f75c73c4bc71e7eb0fd2ce1
vivek kumar <vivit1006117@gmail.com>
2016-12-20 18:05:53 +0530

    added login file and postgres jar

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -1,5 +1,21 @@
 package Login;
 
-public class Login {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+class Login {
+
+    public static void main(String args[]) {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres",
+                            "ganesh", "ganesh");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
 }
